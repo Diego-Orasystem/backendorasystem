86ec8522f2ebc46ef55f24deccbdd7481a856c2c
Diego Enrique Diaz Acosta <166874983+DiegoDuoc7@users.noreply.github.com>
2025-11-24 14:30:08 -0300

    Update database server and credentials in index.js to new values for improved security and functionality

--- index.js
+++ index.js
@@ -148,10 +148,10 @@ app.use(express.static('.')); // Sirve archivos estáticos desde la raíz
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Configuración de la base de datos SQL Server
 const dbConfig = {
-  server: 'orasystembackoffice.database.windows.net',
-  database: 'seguridadorasysbackup',
-  user: 'adminorasystem',
-  password: 'admin2025.',
+  server: 'orasystem2.database.windows.net',
+  database: 'backup11-11-2025ORASYSTEM',
+  user: 'backoffice',
+  password: 'Orasystem.2025',
   options: {
     encrypt: true,
     trustServerCertificate: false,
